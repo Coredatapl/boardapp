d34e2767a4dda1ed11fd78f981a1531ef4dc5d14
Maciej Wasiak <hello@maciejwasiak.com>
2025-01-23 14:57:54 +0100

    #40 Search bar update

--- README.md
+++ README.md
@@ -9,16 +9,18 @@ Board App is a React application that replaces the browser's start page, provide
 ## Main features
 
 - Extended search options
-- Shortcuts of visited pages
+- Shortcuts for favorite pages
 - Current weather informations
 - Current date and time informations
 - Todo list widget
+- Personalised settings
 
-Find out more at [coredata.pl](https://coredata.pl)
+Find out more at [board.coredata.pl](https://board.coredata.pl/)
 
 ## Requirements
 
 - NodeJS **LTS** version from [NodeJs Official Page](https://nodejs.org)
+- NPM Package manager
 
 ## Configuration
 
@@ -54,16 +56,6 @@ The build is minified and the filenames include the hashes.\
 Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-#### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
 
 ### Learn More
 
--- src/components/widgets/search/SearchBar.tsx
+++ src/components/widgets/search/SearchBar.tsx
@@ -6,18 +6,23 @@ import Button from '../../common/Button';
 import SearchEngineSelector from './SearchEngineSelector';
 import QueryHistoryListItem from './QueryHistoryListItem';
 import { SearchEngineEnum, SearchService } from './SearchService';
+import SearchEngineIndicator from './SearchEngineIndicator';
+import { useUi } from '../../../hooks/useUI';
 
+// TODO: One search engine (Chrome Search Api) with enhanced UI
 export default function SearchBar() {
   const { setGlobalState } = useGlobalState();
+  const { animateResize } = useUi();
   const [showQueryHistory, setShowQueryHistory] = useState(false);
   const [similarQueries, setSimilarQueries] = useState<string[]>([]);
   const [focused, setFocused] = useState(false);
   const [query, setQuery] = useState('');
   const [placeHolder, setPlaceHolder] = useState('Search the web...');
   const [searchEngine, setSearchEngine] = useState(SearchEngineEnum.Google);
-  const searchBarRef = useRef<HTMLDivElement>(null);
   const searchService = new SearchService(new CacheApi());
   const minQueryLength = searchService.minQueryLength;
+  const searchBarRef = useRef<HTMLDivElement>(null);
+  const searchButtonsRef = useRef<HTMLDivElement>(null);
 
   function inputFocus() {
     (document.getElementById('searchQueryInput') as HTMLInputElement).focus();
@@ -81,10 +86,21 @@ export default function SearchBar() {
   }
 
   useEffect(() => {
-    setGlobalState((state) => ({
-      ...state,
-      widgetShortcutsHidden: focused || showQueryHistory,
-    }));
+    if (focused) {
+      setGlobalState((state) => ({
+        ...state,
+        widgetShortcutsHidden: true,
+      }));
+      animateResize(searchButtonsRef.current, 500);
+    } else {
+      animateResize(searchButtonsRef.current, 500, true);
+      setTimeout(() => {
+        setGlobalState((state) => ({
+          ...state,
+          widgetShortcutsHidden: focused || showQueryHistory,
+        }));
+      }, 300);
+    }
   }, [focused]);
 
   return (
@@ -93,92 +109,94 @@ export default function SearchBar() {
       ref={searchBarRef}
     >
       <div
-        className={`relative flex w-full z-10 items-center bg-white/70 text-gray-800 hover:bg-gray-100 hover:text-gray-700 backdrop-blur-sm ${
-          showQueryHistory || focused ? 'rounded-t-lg' : 'shadow-lg rounded-lg'
-        } app-state-focused`}
+        className={`relative flex flex-col w-full z-10 items-center bg-white/70 text-gray-800 hover:bg-gray-100 hover:text-gray-700 backdrop-blur-sm shadow-lg rounded-lg app-state-focused`}
       >
-        <div className="w-12 h-full px-3 place-items-center text-gray-700">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
+        <div className="relative flex w-full h-full">
+          <div className="w-12 h-full px-3 place-items-center text-gray-700">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 mt-3"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </div>
+
+          <input
+            className="peer w-full h-full outline-none appearance-none text-lg pr-2 py-3 bg-transparent"
+            id="searchQueryInput"
+            type="text"
+            role="combobox"
+            placeholder={placeHolder}
+            value={query}
+            autoCapitalize="off"
+            autoComplete="off"
+            autoCorrect="off"
+            autoFocus={false}
+            spellCheck={false}
+            aria-autocomplete="both"
+            aria-haspopup={false}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyUp={(e) => handleKeyUp(e)}
+            onFocus={() => {
+              setPlaceHolder('Type what you are looking for and press Enter');
+              setFocused(true);
+            }}
+            onBlur={() => {
+              setPlaceHolder('Search the web...');
+            }}
+          />
+          <SearchEngineIndicator searchEngine={searchEngine} />
         </div>
 
-        <input
-          className="peer w-full h-full outline-none appearance-none text-lg pr-2 py-3 bg-transparent"
-          id="searchQueryInput"
-          type="text"
-          role="combobox"
-          placeholder={placeHolder}
-          value={query}
-          autoCapitalize="off"
-          autoComplete="off"
-          autoCorrect="off"
-          autoFocus={false}
-          spellCheck={false}
-          aria-autocomplete="both"
-          aria-haspopup={false}
-          onChange={(e) => setQuery(e.target.value)}
-          onKeyUp={(e) => handleKeyUp(e)}
-          onFocus={() => {
-            setPlaceHolder('Type what you are looking for and press Enter');
-            setFocused(true);
-          }}
-          onBlur={() => {
-            setPlaceHolder('Search the web...');
-          }}
-        />
-        <SearchEngineSelector
-          searchEngine={searchEngine}
-          setSearchEngine={setSearchEngine}
-        />
-      </div>
+        <div
+          className={`relative flex w-full overflow-hidden items-center bg-white/70 text-gray-800 app-state-focused transition-maxheight duration-500 ease-in-out ${
+            showQueryHistory ? 'max-h-screen z-3' : 'max-h-0 z-0'
+          }`}
+        >
+          <ul className={`relative list-none w-full`}>
+            {similarQueries.map((query, idx) => (
+              <QueryHistoryListItem
+                key={idx}
+                query={query}
+                useSimilarQuery={useSimilarQuery}
+                deleteQuery={deleteQuery}
+              />
+            ))}
+          </ul>
+        </div>
 
-      <div
-        className={`relative flex w-full overflow-hidden items-center bg-white/70 text-gray-800 app-state-focused transition-maxheight duration-500 ease-in-out ${
-          showQueryHistory ? 'max-h-screen z-3' : 'max-h-0 z-0'
-        }`}
-      >
-        <ul className={`relative list-none w-full`}>
-          {similarQueries.map((query, idx) => (
-            <QueryHistoryListItem
-              key={idx}
-              query={query}
-              useSimilarQuery={useSimilarQuery}
-              deleteQuery={deleteQuery}
-            />
-          ))}
-        </ul>
-      </div>
-
-      <div
-        className={`relative flex w-full rounded-b-lg shadow-xl overflow-hidden items-center bg-white/70 text-gray-800 app-state-focused transition-maxheight duration-100 ease-in-out ${
-          focused || showQueryHistory ? 'max-h-screen z-3' : 'max-h-0 z-0'
-        }`}
-      >
-        <div className="relative flex w-full gap-4 p-2 justify-center">
-          <Button
-            title="Search"
-            type="button"
-            onClick={submitQuery}
-            style="md:w-3/12 border border-gray-200 font-semibold shadow-none text-gray-500 bg-gray-100 hover:text-gray-700 hover:bg-gray-200 active:bg-gray-200 active:shadow-none"
-          />
-          <Button
-            type="button"
-            title="Cancel"
-            onClick={cancelQuery}
-            style="md:w-3/12 border border-gray-200 font-semibold shadow-none text-gray-500 bg-gray-100 hover:text-red-700 hover:bg-gray-200 active:bg-gray-200 active:shadow-none"
-          />
+        <div
+          ref={searchButtonsRef}
+          className={`flex w-full h-full max-h-screen overflow-hidden`}
+        >
+          <div
+            className={`relative flex w-full overflow-hidden items-center ${
+              focused || showQueryHistory ? 'z-3' : 'z-0'
+            }`}
+          >
+            <div className="relative flex w-full gap-4 p-2 justify-center">
+              <Button
+                title="Search"
+                type="button"
+                onClick={submitQuery}
+                style="md:w-3/12 border border-gray-200 font-semibold shadow-none text-gray-500 bg-gray-100 hover:text-gray-700 hover:bg-gray-200 active:bg-gray-200 active:shadow-none"
+              />
+              <Button
+                type="button"
+                title="Cancel"
+                onClick={cancelQuery}
+                style="md:w-3/12 border border-gray-200 font-semibold shadow-none text-gray-500 bg-gray-100 hover:text-red-700 hover:bg-gray-200 active:bg-gray-200 active:shadow-none"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
